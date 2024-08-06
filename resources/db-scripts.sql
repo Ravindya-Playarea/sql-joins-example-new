9a4c07f14cbcd19c6dc02c8db3af713c1cf6a2c3
Tharindu Rukshan <tharindurukshan709@gmail.com>
2024-08-06 19:28:36 +0530

    Create Enrolment Table

--- resources/db-scripts.sql
+++ resources/db-scripts.sql
@@ -2,3 +2,11 @@ CREATE TABLE student(
     id VARCHAR(10) PRIMARY KEY,
     name VARCHAR(100) NOT NULL
 );
+
+CREATE TABLE enrolment(
+                          student_id VARCHAR(10) NOT NULL,
+                          batch_number VARCHAR(10) NOT NULL ,
+                          CONSTRAINT pk_enrolment PRIMARY KEY (student_id,batch_number),
+                          CONSTRAINT fk_student FOREIGN KEY (student_id) REFERENCES student (id),
+                          CONSTRAINT fk_batch FOREIGN KEY (batch_number) REFERENCES batch (batch_number)
+);
